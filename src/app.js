8b1032cfee44a3828cbaee62ff8cffd17f5386fc
Cindy Tammelin <100030995+CindyTammelin@users.noreply.github.com>
2025-12-01 00:59:22 +0000

    Generador de cartas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateRandomCard();
 };
+
+function generateRandomCard() {
+  const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  const suits = ['heart', 'spade', 'club', 'diamond'];
+  
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  
+  let suitSymbol;
+  if (randomSuit === 'heart') suitSymbol = '♥';
+  if (randomSuit === 'diamond') suitSymbol = '♦';
+  if (randomSuit === 'spade') suitSymbol = '♠';
+  if (randomSuit === 'club') suitSymbol = '♣';
+  
+  document.querySelector("#app").innerHTML = `
+    <div class="card ${randomSuit}">
+      <div class="top-left">${suitSymbol}</div>
+      <div class="center">${randomValue}</div>
+      <div class="bottom-right">${suitSymbol}</div>
+    </div>
+  `;
+}
